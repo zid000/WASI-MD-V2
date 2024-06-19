eb6e79e9f871760a949a226710f9b31a0a825ccf
THE PROGRAMMER <142110420+Itxxwasi@users.noreply.github.com>
2024-06-19 11:51:49 +0500

    Update config.js

--- config.js
+++ config.js
@@ -51,7 +51,7 @@ global.save_status_from = process.env.SAVE_STATUS_FROM || "";
 global.read_status_from = process.env.READ_STATUS_FROM || "";
 
 global.api_smd = "https://api-smd-1.vercel.app";
-global.scan = "https://wasimdscanner-68feefafb737.herokuapp.com/";
+global.scan = "https://pair-qr-wasi-md.onrender.com";
 
 global.SESSION_ID =
   process.env.SESSION_ID ||
